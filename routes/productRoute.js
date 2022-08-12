555744a7cae594f6c2100087301f14d6736a28b3
Pedro Pereira <pedro.2108@hotmail.com>
2022-08-12 15:10:01 -0300

    feat: Routes to product implemented

---
+++ routes/productRoute.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const productController = require('../controllers/productController');
+
+const productRoute = express.Router();
+
+productRoute.get('/', productController.getAll);
+
+module.exports = productRoute;
