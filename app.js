61109b70ca6b1d02e2d62c9ae648efaf9461bf1c
Pedro Pereira <pedro.2108@hotmail.com>
2022-08-12 15:10:18 -0300

    feat: use /products routes implemented

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', routes.productRoute);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
