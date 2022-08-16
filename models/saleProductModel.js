d74dbf7bc6107df167143d83cadd2008510450e0
Pedro Pereira <pedro.2108@hotmail.com>
2022-08-16 15:19:46 -0300

    refact: Add return true on saleProductModel

--- models/saleProductModel.js
+++ models/saleProductModel.js
@@ -1,9 +1,13 @@
 const connection = require('./connection');
 
-const create = async ({ saleId, productId, quantity }) => connection.execute(
-  'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?);',
-  [saleId, productId, quantity],
-); 
+const create = async ({ saleId, productId, quantity }) => {
+  await connection.execute(
+    'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?);',
+    [saleId, productId, quantity],
+  );
+  
+  return true;
+};
 
 module.exports = {
   create,
