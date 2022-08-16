df996dc7a8020a1a1d98c47de2752855f0e5e4ee
Pedro Pereira <pedro.2108@hotmail.com>
2022-08-16 15:54:48 -0300

    feat: Model to get all sales and by id implemented

--- models/saleProductModel.js
+++ models/saleProductModel.js
@@ -1,5 +1,12 @@
 const connection = require('./connection');
 
+const serialize = (sale) => ({
+  saleId: sale.sale_id,
+  date: sale.date,
+  productId: sale.product_id,
+  quantity: sale.quantity,
+});
+
 const create = async ({ saleId, productId, quantity }) => {
   await connection.execute(
     'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?);',
@@ -9,6 +16,45 @@ const create = async ({ saleId, productId, quantity }) => {
   return true;
 };
 
+const getAll = async () => {
+  const [result] = await connection.execute(
+    `SELECT
+      sp.sale_id, sp.product_id, sp.quantity, s.date
+    FROM
+      StoreManager.sales_products AS sp
+    INNER JOIN
+      StoreManager.sales AS s
+    ON
+      sp.sale_id = s.id
+    ORDER BY
+      sp.sale_id ASC, sp.product_id ASC;`,
+  );
+
+  return result.map(serialize);
+};
+
+const getById = async (id) => {
+  const [result] = await connection.execute(
+    `SELECT
+      sp.product_id, sp.quantity, s.date
+    FROM
+      StoreManager.sales_products AS sp
+    INNER JOIN
+      StoreManager.sales AS s
+    ON
+      sp.sale_id = s.id
+    WHERE 
+      sp.sale_id = ?
+    ORDER BY
+      sp.sale_id ASC, sp.product_id ASC;`,
+    [id],
+  );
+
+  return result.map(serialize);
+};
+
 module.exports = {
   create,
+  getAll,
+  getById,
 };
