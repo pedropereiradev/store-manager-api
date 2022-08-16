1b3b6d8372fbffddcf20a3cfd40e1cf5a2b3816e
Pedro Pereira <pedro.2108@hotmail.com>
2022-08-16 15:55:15 -0300

    feat: Controller to get all sales and by id implemented

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -12,6 +12,26 @@ const create = async (req, res) => {
   return res.status(201).json(result);
 };
 
+const getAll = async (req, res) => {
+  const result = await saleService.getAll();
+
+  return res.status(200).json(result);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await saleService.getById(id);
+
+  if (result.error) {
+    return res.status(result.error.code).json({ message: result.error.message });
+  }
+
+  return res.status(200).json(result);
+};
+
 module.exports = {
   create,
+  getAll,
+  getById,
 };
