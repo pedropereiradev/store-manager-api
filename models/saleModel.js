05e454072f2cb54cf587b6165f5ff5be19fcdbb4
Pedro Pereira <pedro.2108@hotmail.com>
2022-08-16 21:46:20 -0300

    feat: Model for delete sale implemented

--- models/saleModel.js
+++ models/saleModel.js
@@ -6,6 +6,18 @@ const create = async () => {
   return { id: result.insertId };
 };
 
+const destroy = async (id) => {
+  await connection.execute(
+    `DELETE FROM
+      StoreManager.sales
+    WHERE
+      id = ?;
+    `,
+    [id],
+  );
+};
+
 module.exports = {
   create,
+  destroy,
 };
